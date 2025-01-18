3f4d9f6940e74af5ddf4d6b01370723469700a26
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-18 12:46:07 +0530

    Change to pure react

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-center font-bold animate-bounce text-[64px] lg:text-[128px] sm:text-[48px]">
-        Coming Soon......!
-      </h1>
-    </div>
-  );
-}
