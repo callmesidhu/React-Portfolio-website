5af9473296ee42430d98ff2652fa897e5346aba6
S SIDHARTH <sidharthpunalur@gmail.com>
2025-02-15 10:03:29 +0530

    tailwind bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="bg-red-900 text-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Hello, Tailwind with React! 👋</h1>
+      <h1 className="text-4xl font-bold mb-4 text-red-500">Hello, Tailwind with React! 👋</h1>
     </div>
   );
 };
